18a4b00d0bf8b354802b698a7aa366812d445527
unit-1m <atwo80854@gmail.com>
2025-04-17 12:10:50 +0900

    .

--- src/database/GOODS_DATA.tsx
+++ src/database/GOODS_DATA.tsx
@@ -1,15 +1,47 @@
 import { GoodsData } from './GoodsData'
-import edge_reb from '/goods/cyber-edge 800x.png'
-import eva_gun_sword from '/goods/eva-gun-sword 800x.jpg'
-import eva_asuka_sitting from '/goods/asuka_sitting 800x.jpg'
-import eva_water from '/goods/eva-water p.card.png'
-import gh_asuka from '/goods/gh-asuka.jpg'
-import gh_badge_asuka from '/goods/gh_badge_asuka.jpg'
-import gh_badge_rei from '/goods/gh_badge_rei.jpg'
-import gh_rei from '/goods/gh-rei.jpg'
+
+import edge_reb from '/goods/CPE-RBC-VRT.png'
+import eva_gun_sword from '/goods/EVA-ASKREI-GNSW.jpg'
+import eva_asuka_sitting from '/goods/EVA-ASK-SIT.jpg'
+import eva_water from '/goods/EVA-ASK-LAK.png'
+import gh_asuka from '/goods/EVA-ASK-SWM.jpg'
+import gh_badge_asuka from '/goods/EVA-ASK-SQR.jpg'
+import gh_badge_rei from '/goods/EVA-REI-SQR.jpg'
+import gh_rei from '/goods/EVA-REI-FLR.jpg'
+
+import eva_asuka_casino from '/goods/EVA-ASK-CSN.jpg'
+import eva_rei_casino from '/goods/EVA-REI-CSN.jpg'
+
+import eva_asuka_box from '/goods/EVA-ASK-BOX.jpg'
+import eva_rei_box from '/goods/EVA-REI-BOX.jpg'
+
+import eva_asuka_cartoon from '/goods/EVA-ASK-CTN.jpg'
+import eva_rei_cartoon from '/goods/EVA-REI-CTN.jpg'
+
+import slg_sjwe_crt from '/goods/SLG-SJWE-CRT.jpg'
+import slg_sjws_crt from '/goods/SLG-SJWS-CRT.jpg'
+import slg_brr_crt from '/goods/SLG-BRR-CRT.jpg'
+import slg_igr_crt from '/goods/SLG-IGR-CRT.jpg'
+
+import eva_ask_corot from '/goods/EVA-ASK-CRT.jpg'
+import eva_rei_corot from '/goods/EVA-REI-CRT.jpg'
+import eva_kao_corot from '/goods/EVA-KAO-CRT.jpg'
+
+import pnsg_vrt from '/goods/PSG-STK-VRT.jpg'
+
 import place_holder from '/goods/goods-img-placeholder.jpg'
 
 export const GOODS_DATA: GoodsData[] = [
+  {
+    path: eva_asuka_casino,
+    label: '아스카 카지노',
+    authors: ['SRPNT'],
+  },
+  {
+    path: eva_rei_casino,
+    label: '레이 카지노',
+    authors: ['SRPNT'],
+  },
   {
     path: edge_reb,
     label: '레베카',
@@ -51,48 +83,63 @@ export const GOODS_DATA: GoodsData[] = [
     authors: ['G-HYI'],
   },
   {
-    path: place_holder,
+    path: eva_asuka_cartoon,
     label: '아스카 카툰',
     authors: ['SRPNT'],
   },
   {
-    path: place_holder,
+    path: eva_rei_cartoon,
     label: '레이 카툰',
     authors: ['SRPNT'],
   },
   {
-    path: place_holder,
-    label: '아스카 카지노',
-    authors: ['SRPNT'],
-  },
-  {
-    path: place_holder,
-    label: '레이 카지노',
-    authors: ['SRPNT'],
-  },
-  {
-    path: place_holder,
+    path: eva_asuka_box,
     label: '아스카 박스',
     authors: ['SRPNT'],
   },
   {
-    path: place_holder,
+    path: eva_rei_box,
     label: '레이 박스',
     authors: ['SRPNT'],
   },
   {
-    path: place_holder,
+    path: pnsg_vrt,
     label: '아나키 스타킹',
     authors: ['SRPNT'],
   },
   {
-    path: place_holder,
-    label: '에반게리온 아크릴 코롯토',
+    path: eva_ask_corot,
+    label: '에반게리온 아스카 코롯토',
     authors: ['SRPNT'],
   },
   {
-    path: place_holder,
-    label: '나혼렙 아크릴 코롯토',
+    path: eva_kao_corot,
+    label: '에반게리온 레이 코롯토',
+    authors: ['SRPNT'],
+  },
+  {
+    path: eva_rei_corot,
+    label: '에반게리온 카오루 코롯토',
+    authors: ['SRPNT'],
+  },
+  {
+    path: slg_sjws_crt,
+    label: '나혼렙 성진우S 코롯토',
+    authors: ['WASAK'],
+  },
+  {
+    path: slg_sjwe_crt,
+    label: '나혼렙 성진우E 코롯토',
+    authors: ['WASAK'],
+  },
+  {
+    path: slg_brr_crt,
+    label: '나혼렙 베르 코롯토',
+    authors: ['WASAK'],
+  },
+  {
+    path: slg_igr_crt,
+    label: '나혼렙 이그리트 코롯토',
     authors: ['WASAK'],
   },
   {
